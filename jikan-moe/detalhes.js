a2a2b77bf3ebb25cb9bb4715d0f3aa6af120e82a
Wendeley <wendeleyf@gmail.com>
2022-03-07 17:00:19 -0300

    Add função de preencher os personagens dinamicamente

--- jikan-moe/detalhes.js
+++ jikan-moe/detalhes.js
@@ -1,8 +1,30 @@
+const base_url = 'https://api.jikan.moe/v4/anime';
+
+
 function pageLoad(){
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const id = urlParams.get('id')
-  console.log(id);
+  const id = urlParams.get('id');
+  getChars(id);
  }
+
+ function getChars(id){
+  fetch(`${base_url}/${id}/characters`)
+  .then(response => response.json())
+  .then(updateDom)
+  .catch(error => console.warn(error.message))
+}
+
+function updateDom(characters){
+  console.log(characters)
+  const container = document.querySelector('.container');
+  container.innerHTML = characters.data
+  .map(character => {
+    return `
+      <h5>${character.character.name}</h5>
+      <img src="${character.character.images.webp.image_url}" />
+    `
+  }).join("")
+}
 
 window.addEventListener("load", pageLoad);
